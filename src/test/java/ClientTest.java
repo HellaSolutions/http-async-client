c3ddcd73f4881fdd02e07071419c33653dc5ea9a
Paola Greco <paola.greco@telegraph.co.uk>
2019-11-10 08:39:33 +0000

    commit

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -8,9 +8,7 @@ import uk.co.telegraph.content.api.filter.Query;
 
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 
 import static org.asynchttpclient.Dsl.*;
 
@@ -24,21 +22,24 @@ public class ClientTest {
         try(AsyncHttpClient asyncHttpClient = asyncHttpClient()){
 
 
-            CompletableFuture<Response> whenResponse = asyncHttpClient.
+            ListenableFuture<Response> whenResponse = asyncHttpClient.
                     preparePost("http://localhost:8080/content/v3/content/scroll").
                     setBody(gson.toJson(Query.all())).
                     setHeader("Content-type", "application/json").
-                    execute().
-                    toCompletableFuture().
-                    exceptionally(t -> {
-                        t.printStackTrace();
-                        return null;
-                    }).thenApply(r -> {
-                        System.out.println(r);
-                        return r;
-            });
-            whenResponse.get();
-            //whenResponse.join();
+                    execute();
+
+            Runnable callback = () -> {
+                try  {
+                    Response response = whenResponse.get();
+                    System.out.println(response);
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
+                }
+            };
+
+            Executor executor = Executors.newSingleThreadExecutor();
+            whenResponse.addListener(callback, executor);
+
 
             Thread.sleep(500000);
 
